6c23013227560c61aa782d2702b75362c6e42415
pedrohgarbim <phg.garbim@gmail.com>
2024-09-03 00:01:55 -0300

    Models, Repositories

--- EmployeeManagement/Models/Employee.cs
+++ EmployeeManagement/Models/Employee.cs
@@ -8,6 +8,8 @@
         public string Email { get; set; }
         public string Phone { get; set; }
         public string Position { get; set; }
-        
+
+        public string FullName => $"{FirstName} {LastName}";
+
     }
 }
---
+++ EmployeeManagement/Repositories/IEmployeeRepository.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeManagement.Models;
+
+namespace EmployeeManagement.Repositories
+{
+    // CRUD
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllAsync(); // R read
+        Task<Employee?> GetByIdAsync(int id); // R read
+        Task<Employee?> GetByNameAsync(string name); // R read
+        Task AddEmployeeAsync(Employee employee); // C create
+        Task UpdateEmployeeAsync(Employee employee); // U update
+        Task DeleteEmployeeAsync(int id); // D delete
+
+    }
+}
+// we use interfaces to create specific
+// contracts so that a class which implements an interface has
+// to implement all the members of that interface.
+//So that we can have type safety and guarantee.
+// we have not implemented them yet,here we defined them because we are in a interface
+
