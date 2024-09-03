c0e3d0188380c0daad027e47525709d2721a3ee2
pedrohgarbim <phg.garbim@gmail.com>
2024-09-02 22:49:39 -0300

    Cors and dbcontext

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -1,3 +1,6 @@
+using EmployeeManagement.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
 namespace EmployeeManagement
 {
     public class Program
@@ -5,7 +8,23 @@ namespace EmployeeManagement
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddDbContext<AppDbContext>(
+                options => options.UseInMemoryDatabase("EmployeeDb"));
+
+            builder.Services.AddCors(options =>
+            {
+                options.AddPolicy("Mycors", builder =>
+                {
+                    builder.WithOrigins("http://localhost:4200")
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+                });
+            });
+
             var app = builder.Build();
+
+            app.UseCors("Mycors");
 
             app.MapGet("/", () => "Hello World!");
 
